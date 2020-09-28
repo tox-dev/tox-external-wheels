5388a5d56393e24599cd0a45d3b5536d647e3496
Mark Keller <contact@markkeller.dev>
2020-09-28 01:35:25 -0700

    add a bogus dependency to test dependabot

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from setuptools import setup
 
-setup(use_scm_version={"write_to": "src/tox_external_wheels/version.py"})
+setup(
+    use_scm_version={"write_to": "src/tox_external_wheels/version.py"},
+    install_requires=["tox>=3.12.2", "six<1.0.0"],
+)
